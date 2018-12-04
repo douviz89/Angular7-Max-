64f3bbb44c7b1b0a239e66c8435d8fc82c4fd586
ZEMOURI Hocem <hocem.zemouri-extern@renault.com>
2018-12-04 18:52:47 +0100

    first commit - event binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { MessagesComponent } from './messages/messages.components';
+import { WarningComponent } from './messages/warning/warning.component';
+import { SuccessComponent } from './messages/success/success.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    MessagesComponent,
+    WarningComponent,
+    SuccessComponent
   ],
   imports: [
     BrowserModule
